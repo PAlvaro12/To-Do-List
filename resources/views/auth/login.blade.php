9ac9de9f932294182761a554fb6a01fcda5df1bd
PAlvaro12 <202012669a@utea.edu.pe>
2024-08-11 14:51:26 -0500

    Login con registro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,51 +15,64 @@
             rel="stylesheet"
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
             crossorigin="anonymous">
-            
-            <link rel="stylesheet" herf="{{ asset('assets/login.css')}}">
-        
+        <link rel ="stylesheet" herf="{{ asset('assets/login,css')}}">
+    />
     </head>
 
     <body>
-        <section class="vh-100 gradient-custom">
+        <section class="h-100 gradient-form" style="background-color: #eee;">
             <div class="container py-5 h-100">
                 <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                    <div class="card bg-dark text-white" style="border-radius: 1rem;">
-                    <div class="card-body p-5 text-center">
+                <div class="col-xl-10">
+                    <div class="card rounded-3 text-black">
+                    <div class="row g-0">
+                        <div class="col-lg-6">
+                        <div class="card-body p-md-5 mx-md-4">
 
-                        <div class="mb-md-5 mt-md-4 pb-5">
+                            <div class="text-center">
+                            <img src="{{ asset('assets/logo.png')}}"
+                                style="width: 185px;" alt="logo">
+                            <h4 class="mt-1 mb-5 pb-1">LOGIN </h4>
+                            </div>
 
-                        <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-                        <p class="text-white-50 mb-5">Please enter your login and password!</p>
+                            <form action="{{route('login')}}" method="post">
+                                @csrf
+                                <p>Iniciar Sesión</p>
 
-                        <div data-mdb-input-init class="form-outline form-white mb-4">
-                            <input type="email" id="typeEmailX" class="form-control form-control-lg" />
-                            <label class="form-label" for="typeEmailX">Email</label>
+                                <div data-mdb-input-init class="form-outline mb-4">
+                                    <label class="form-label" for="form2Example11">Correo</label>
+                                    <input type="email"  name="email" id="form2Example11" class="form-control"
+                                    placeholder="Ingresar correo" />
+                                </div>
+
+                                <div data-mdb-input-init class="form-outline mb-4">
+                                    <label class="form-label" for="form2Example22">Contraseña</label>
+                                    <input type="password" name="password" id="form2Example22" class="form-control" />
+                                    
+                                </div>
+
+                                <div class="text-center pt-1 mb-5 pb-1">
+                                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit">Ingresar</button>
+                                    
+                                </div>
+
+                                <div class="d-flex align-items-center justify-content-center pb-4">
+                                    <p class="mb-0 me-2">no tienes cuenta ?</p>
+                                    <a href="{{route('register')}}" class="btn btn-outline-danger">Crear cuenta</a>
+                                </div>
+
+                            </form>
+
                         </div>
-
-                        <div data-mdb-input-init class="form-outline form-white mb-4">
-                            <input type="password" id="typePasswordX" class="form-control form-control-lg" />
-                            <label class="form-label" for="typePasswordX">Password</label>
                         </div>
-
-                        <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
-
-                        <button data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
-
-                        <div class="d-flex justify-content-center text-center mt-4 pt-1">
-                            <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
-                            <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                            <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                        <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                        <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                            <h4 class="mb-4">Tarea Generado</h4>
+                            <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         </div>
-
                         </div>
-
-                        <div>
-                        <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
-                        </p>
-                        </div>
-
                     </div>
                     </div>
                 </div>
